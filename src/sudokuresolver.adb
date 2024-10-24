dd8106b206a0c6b1908fe87470a1887b1acfab8a
Olivier Boite <166839849+olivierboite@users.noreply.github.com>
2024-10-24 19:26:27 +0200

    Update sudokuresolver.adb

--- src/sudokuresolver.adb
+++ src/sudokuresolver.adb
@@ -1,12 +1,15 @@
 with Resolution;
 with Ada.Command_Line;
 
-procedure sudokuResolver is
+--main program
+procedure sudokuresolver is
   debug : boolean := False;
 begin
    if Ada.Command_Line.Argument_Count > 0
-   then if Ada.Command_Line.Argument(1)= "-d" then debug:= True; end if;
+   then if Ada.Command_Line.Argument(1)= "-d" 
+        then debug:= True; 
+        end if;
    end if;
 
    Resolution.resoud(debug);
-end sudokuResolver;
+end sudokuresolver;
